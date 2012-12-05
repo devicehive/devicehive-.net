5cd131526004d4ec0e01c10a3b9742b9b9d10e1f
aorel <aorel@dataart.com>
2012-12-05 14:40:13 +0400

    Added service installer

---
+++ src/Server/DeviceHive.WebSockets/WebSocketServiceInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace DeviceHive.WebSockets
+{
+    [RunInstaller(true)]
+    public class WebSocketServiceInstaller : Installer
+    {
+        public WebSocketServiceInstaller()
+        {
+            Installers.AddRange(new Installer[]
+            {
+                new ServiceProcessInstaller(),
+                new ServiceInstaller() {ServiceName = "DeviceHive.WebSockets"}
+            });
+        }
+    }
+}
