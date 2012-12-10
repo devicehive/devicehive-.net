f621b9c942cbe8cba7c5cb33f453ea2f2ed5efe5
aorel <aorel@dataart.com>
2012-12-10 14:23:28 +0400

    Build script modification for web sockets project

--- src/Server/build.cmd
+++ src/Server/build.cmd
@@ -2,3 +2,4 @@ C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe DeviceHive.Server.sln 
 C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe DeviceHive.API\DeviceHive.API.csproj /t:ResolveReferences;_WPPCopyWebApplication /p:Configuration=Release /p:BuildingProject=true;CleanWebProjectOutputDir=True;WebProjectOutputDir=..\..\..\bin\Server\Web /clp:Verbosity=Quiet
 rmdir ..\..\bin\Server\DBMigrator /s /q
 xcopy DeviceHive.DBMigrator\bin\Release ..\..\bin\Server\DBMigrator /e /y /i
+xcopy DeviceHive.WebSockets\bin\Release ..\..\bin\Server\WebSockets /e /y /i
