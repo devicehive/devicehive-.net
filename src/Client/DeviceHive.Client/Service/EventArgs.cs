300d8ac29ab6b51f6d5e7f2bb39adf2e7b980b4d
aorel <aorel@dataart.com>
2012-12-11 15:06:29 +0400

    Web socket client service implemented

---
+++ src/Client/DeviceHive.Client/Service/EventArgs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DeviceHive.Client
+{
+    /// <summary>
+    /// Provides information about <see cref="Notification"/> related event.
+    /// </summary>
+    public class NotificationEventArgs : EventArgs
+    {
+        private readonly Notification _notification;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="notification">Notification object</param>
+        public NotificationEventArgs(Notification notification)
+        {
+            _notification = notification;
+        }
+
+        /// <summary>
+        /// Gets notification object
+        /// </summary>
+        public Notification Notification
+        {
+            get { return _notification; }
+        }
+    }
+
+    /// <summary>
+    /// Provides information about <see cref="Command"/> related event.
+    /// </summary>
+    public class CommandEventArgs : EventArgs
+    {
+        private readonly Command _command;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="command">Command object</param>
+        public CommandEventArgs(Command command)
+        {
+            _command = command;
+        }
+
+        /// <summary>
+        /// Gets command object
+        /// </summary>
+        public Command Command
+        {
+            get { return _command; }
+        }
+    }
+}
