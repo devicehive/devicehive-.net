a971a92797800bef75fdd9f9180b5c6c640884ab
aorel <aorel@dataart.com>
2012-12-13 18:21:21 +0400

    Small fixes

--- src/Server/DeviceHive.WebSockets/Properties/AssemblyInfo.cs
+++ src/Server/DeviceHive.WebSockets/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DeviceHive.WebSockets")]
+[assembly: AssemblyTitle("DeviceHive.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DeviceHive.WebSockets")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCompany("DataArt Apps")]
+[assembly: AssemblyProduct("DeviceHive")]
+[assembly: AssemblyCopyright("Copyright © DataArt Apps 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
