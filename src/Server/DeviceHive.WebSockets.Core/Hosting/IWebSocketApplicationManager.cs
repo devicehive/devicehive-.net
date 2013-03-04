ebaab25a78742d9af761d78594a8e7f24694434d
aorel <aorel@dataart.com>
2013-03-04 14:17:28 +0400

    Missed IWebSocketApplicationManager.cs

---
+++ src/Server/DeviceHive.WebSockets.Core/Hosting/IWebSocketApplicationManager.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceModel;
+
+namespace DeviceHive.WebSockets.Core.Hosting
+{
+    [ServiceContract]
+    public interface IWebSocketApplicationManager
+    {
+        [OperationContract]
+        void AddApplication(string host, string exePath, string commandLineArgs,
+            string userName, string userPassword);
+
+        [OperationContract]
+        void RemoveApplication(string host);
+
+        [OperationContract]
+        void StopApplication(string host);
+
+        [OperationContract]
+        void StartApplication(string host);
+    }
+}
