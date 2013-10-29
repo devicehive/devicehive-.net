ae83b15fc7f645722d04597a470d84990e4d11dd
Dmitry Vyrostkov <diman8@gmail.com>
2013-10-29 19:10:17 +0400

    Added support for notification/command subscriptions by name via WebSocket.

--- src/Server/DeviceHive.WebSockets.API/Subscriptions/Subscription.cs
+++ src/Server/DeviceHive.WebSockets.API/Subscriptions/Subscription.cs
@@ -4,14 +4,17 @@ namespace DeviceHive.WebSockets.API.Subscriptions
 {
     public class Subscription<TKey>
     {
-        public Subscription(TKey key, WebSocketConnectionBase connection)
+        public Subscription(TKey key, WebSocketConnectionBase connection, object data = null)
         {
             Key = key;
             Connection = connection;
+            Data = data;
         }
 
         public TKey Key { get; private set; }
 
         public WebSocketConnectionBase Connection { get; set; }
+
+        public object Data { get; set; }
     }
 }
