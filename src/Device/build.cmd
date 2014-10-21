79a7dc09fc59c58c73f9781e61529496c243a35a
Dmitry Vyrostkov <diman8@gmail.com>
2014-10-21 21:59:25 +0400

    Switching to newer NuGet package restore model.

--- src/Device/build.cmd
+++ src/Device/build.cmd
@@ -1,3 +1,5 @@
+"../../tools/nuget.exe" restore
+
 C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe DeviceHive.Device.sln /p:Configuration=Release /clp:Verbosity=Quiet
 rmdir ..\..\Bin\Device /s /q
 xcopy DeviceHive.Device\bin\Release ..\..\bin\Device /e /y /i
