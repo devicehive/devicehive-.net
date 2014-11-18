e9a99d315c2f2baa78058d4eb1014c5a795785b5
Dmitry Vyrostkov <diman8@gmail.com>
2014-11-18 21:49:36 +0300

    Added BinaryClient example.

---
+++ src/Device/Examples/BinaryClient/BinaryServiceHelper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using DeviceHive.Device;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace DeviceHive.Binary
+{
+    public class BinaryServiceHelper : BinaryServiceBase
+    {
+        private Dictionary<int, TaskCompletionSource<CommandResult>> _cmdData;
+        private const int _timeout = 10000;
+        private int _cmdId;
+        private TaskCompletionSource<DeviceRegistrationInfo> _regComplete;
+        
+        public struct CommandResult
+        {
+            public string Status;
+            public string Result;
+            public long ExecutionTime;
+        };
+
+        public BinaryServiceHelper(IBinaryConnection connection)
+            : base(connection)
+        {
+            _cmdId = 0;
+            _cmdData = new Dictionary<int, TaskCompletionSource<CommandResult>>();
+        }
+
+        public override void Start()
+        {
+            _regComplete = new TaskCompletionSource<DeviceRegistrationInfo>();
+            base.Start();
+            RequestRegistration();
+            _regComplete.Task.Wait();
+        }
+
+        public async Task<CommandResult> SendCmd(string command, object arg)
+        {
+            int cid = Interlocked.Increment(ref _cmdId);
+
+            Command cmd = new Command()
+            {
+                Id = cid,
+                Name = command
+            };
+            cmd.Parameters = Newtonsoft.Json.Linq.JToken.FromObject(arg);
+            Stopwatch sw = new Stopwatch();
+
+            TaskCompletionSource<CommandResult> tks = new TaskCompletionSource<CommandResult>();
+            _cmdData.Add(cid, tks);
+
+            sw.Start();
+
+            base.SendCommand(cmd);
+
+            if (await Task.WhenAny(tks.Task, Task.Delay(_timeout)) != tks.Task)
+            {
+                throw new Exception("Timeout");
+            }
+
+            CommandResult rv = await tks.Task;
+            sw.Stop();        
+            
+            _cmdData.Remove(cid);
+            rv.ExecutionTime = sw.ElapsedMilliseconds;
+            return rv;
+        }
+
+        protected override void HandleHotification(DeviceHive.Device.Notification notification)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void NotifyCommandResult(int commandId, string status, string result)
+        {
+            try
+            {
+                TaskCompletionSource<CommandResult> cr = _cmdData[commandId];
+                CommandResult rv = new CommandResult()
+                {
+                    Result = result,
+                    Status = status
+                };
+
+                cr.SetResult(rv);
+            }
+            catch (Exception ) { } // if a command with unknown id comes in - we aren't interested in it
+        }
+
+        protected override void RegisterDevice(DeviceRegistrationInfo registrationInfo)
+        {
+            _regComplete.SetResult(registrationInfo);
+        }
+    }
+ }
---
+++ src/Device/Examples/BinaryClient/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using DeviceHive.Device;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using DeviceHive.Binary;
+
+namespace DeviceHiveTest
+{
+    public struct CubePixel
+    {
+        public byte X;
+        public byte Y;
+        public byte Z;
+        public byte R;
+        public byte G;
+        public byte B;
+    }
+
+    public struct CubeColor
+    {
+        public byte R;
+        public byte G;
+        public byte B;
+    }
+
+    class Program
+    {
+        static async Task TestPixel(BinaryServiceHelper svc)
+        {
+            // Pixel
+            Console.Write("Setting pixel... ");
+            CubePixel cp = new CubePixel
+            {
+                X = 0,
+                Y = 0,
+                Z = 0,
+                R = 0xFF,
+                G = 0,
+                B = 0
+            };
+
+            // C5 C3 01 00 0A 00 01 01 01 00 00 00 00 00 00 FF 00 00 6A
+            BinaryServiceHelper.CommandResult cr = await svc.SendCmd("pixel", cp);
+            Console.WriteLine(cr.Result + " " + cr.ExecutionTime.ToString() + " ms.");
+            Thread.Sleep(1000);
+            
+        }
+
+        static async Task TestFill(BinaryServiceHelper svc, CubeColor clr)
+        {
+            // Fill
+            Console.Write("Setting the cube color... ");
+            BinaryServiceHelper.CommandResult cr = await svc.SendCmd("fill", clr);
+            Console.WriteLine(cr.Result + " " + cr.ExecutionTime.ToString() + " ms.");
+            Thread.Sleep(1000);
+        }
+
+        static async Task TestPixels(BinaryServiceHelper svc)
+        {
+            // Pixels
+            Console.Write("Setting random pixels... ");
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            //while (!Console.KeyAvailable)
+            //{
+
+            CubePixel[] pxs = new CubePixel[4 * 4 * 4];
+
+            for (byte x = 0; x < 4; x++)
+            {
+                for (byte y = 0; y < 4; y++)
+                {
+                    for (byte z = 0; z < 4; z++)
+                    {
+                        CubePixel cpt = new CubePixel()
+                        {
+                            X = (byte)x,
+                            Y = (byte)y,
+                            Z = (byte)z,
+                            R = (byte)rnd.Next(0, 255),
+                            G = (byte)rnd.Next(0, 255),
+                            B = (byte)rnd.Next(0, 255)
+                        };
+
+                        pxs[x * 16 + y * 4 + z] = cpt;
+                    }
+                }
+            }
+            BinaryServiceHelper.CommandResult cr = await svc.SendCmd("pixels", pxs);
+            Console.WriteLine(cr.Result + " " + cr.ExecutionTime.ToString() + " ms.");
+            Thread.Sleep(1000);
+        }
+
+        static void Main(string[] args)
+        {
+            SerialPort com = new SerialPort("COM3", 19200, Parity.None, 8, StopBits.One);
+            com.ReadTimeout = 100;
+            SerialPortBinaryConnection sbc = new SerialPortBinaryConnection(com);
+            BinaryServiceHelper svc = new BinaryServiceHelper(sbc);
+
+            Console.Write("Initializing... ");
+            while (!svc.IsStarted)
+            {
+                try
+                {
+                    svc.Start();
+                }
+                catch (Exception)
+                {
+                }
+            }
+            Console.WriteLine("done.");
+
+            try
+            {
+                TestPixel(svc).Wait();
+
+                TestFill(svc, new CubeColor()
+                    {
+                        R = 0,
+                        G = 0xFF,
+                        B = 0
+                    }).Wait();
+                TestPixels(svc).Wait();
+
+                TestFill(svc, new CubeColor()
+                {
+                    R = 0,
+                    G = 0,
+                    B = 0
+                }).Wait();
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine( ex.InnerException.ToString());
+                Console.ReadKey();
+            }
+            
+            svc.Stop();
+            svc.Dispose();
+            sbc.Dispose();
+            com.Dispose();
+        }
+    }
+}
