d9054496554f0b3505250b84ecaadcfef633fe04
Dmitry Vyrostkov <diman8@gmail.com>
2015-01-08 14:25:41 +0300

    Respond with proper JSON in case of error messages (fixes #6)

--- src/Server/DeviceHive.API/Filters/HandleExceptionAttribute.cs
+++ src/Server/DeviceHive.API/Filters/HandleExceptionAttribute.cs
@@ -1,19 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http.Filters;
-using DeviceHive.API.Models;
+﻿using DeviceHive.API.Models;
 using DeviceHive.Core.Mapping;
 using log4net;
+using System.Net;
+using System.Net.Http;
+using System.Web.Configuration;
+using System.Web.Http;
+using System.Web.Http.Filters;
 
 namespace DeviceHive.API.Filters
 {
     public class HandleExceptionAttribute : ExceptionFilterAttribute
     {
+        private static bool _isCustomError;
+
+        static HandleExceptionAttribute()
+        {
+            var ceSection = (CustomErrorsSection)WebConfigurationManager.OpenWebConfiguration("~").GetSection("system.web/customErrors");
+            _isCustomError = ceSection.Mode != CustomErrorsMode.Off;
+        }
+
         public override void OnException(HttpActionExecutedContext context)
         {
+            if (context.Exception is HttpResponseException)
+                return;
+
             if (context.Exception is JsonMapperException)
             {
                 context.Response = context.Request.CreateResponse<ErrorDetail>
@@ -22,6 +32,8 @@ namespace DeviceHive.API.Filters
             }
 
             LogManager.GetLogger("DeviceHive.API").Fatal("API Exception!", context.Exception);
+            context.Response = context.Request.CreateResponse<ErrorDetail>(HttpStatusCode.InternalServerError,
+                new ErrorDetail(_isCustomError ? "An error has occured with the server" : context.Exception.ToString()));
         }
     }
 }
